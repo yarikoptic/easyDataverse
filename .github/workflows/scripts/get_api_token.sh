fd3702b0e6d89008d5d583fa1939cff5df058bcd
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-08-30 22:33:24 +0200

    Update get_api_token.sh

--- .github/workflows/scripts/get_api_token.sh
+++ .github/workflows/scripts/get_api_token.sh
@@ -4,13 +4,14 @@
 curl -X PUT -d $BUILTIN_USERS_KEY $SERVER_URL/api/admin/settings/BuiltinUsers.KEY && echo "Added BuiltinUsers.KEY"
 
 # Create a user and receive the API Token
-USER_DATA=$( \
-    curl -d @.github/workflows/assets/user.json \
+curl -d @.github/workflows/assets/user.json \
     -H "Content-type:application/json" \
     -o ./user_data.json \
-    "$SERVER_URL/api/builtin-users?password=$NEWUSER_PASSWORD&key=$BUILTIN_USERS_KEY"
-)
+    "$SERVER_URL/api/builtin-users?password=$NEWUSER_PASSWORD&key=$BUILTIN_USERS_KEY" \
+    && echo "Created user"
 
-# Print the API Token
+# Retrieve the API Token
+USER_DATA=$(cat ./user_data.json)
+
 echo $USER_DATA
 echo $USER_DATA | jq -r '.apiToken'
